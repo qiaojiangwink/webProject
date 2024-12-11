1b61d068a8be9f32b120d836b4c631df11fededd
JR <qiaojiangwink@163.com>
2024-12-11 14:42:18 +0800

    前后端联调test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# web-yt
+# web-project
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    proxy: {
+      '/dev-api': {
+        target: 'http://localhost:8088', // 后端服务地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev-api': '', // 将 '/dev-api' 替换为空
+        },
+      },
+    },
+    port: 80, // 前端开发服务器端口
+  },
+});
