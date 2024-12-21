e6db96951272abfd783dd2141325b9941e556aa5
JR <qiaojiangwink@163.com>
2024-12-21 08:40:19 +0800

    Changes

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/dev-api': {
-        target: 'http://localhost:8088', // 后端服务地址
+        target: 'http://localhost:8068', // 后端服务地址
         changeOrigin: true,
         pathRewrite: {
           '^/dev-api': '', // 将 '/dev-api' 替换为空
